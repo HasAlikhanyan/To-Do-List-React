744c851ff9bed7a2ad7d457af4680e872b5ef56c
Hasmik <[hasmik.alikhanyan1@gmail.com]>
2023-03-18 14:34:40 +0400

    add modals

--- src/components/editableTask/EditableTask.jsx
+++ src/components/editableTask/EditableTask.jsx
@@ -1,44 +1,48 @@
+import { Component } from 'react';
 import {Button, Form} from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
 
 import styles from './editableTask.module.css'
 
-function EditableTask(props) {
-    return (
-        <Modal
-            size="md"
-            show={props.isOpenModal}
-        >
-            <Modal.Header 
-                closeButton
-                onClick={props.hideModal}
+class EditableTask extends Component {
+    render() {
+        return (
+            <Modal
+                size="md"
+                show={this.props.isOpenModal}
             >
-                <Modal.Title className={styles.title}>Input changes</Modal.Title>
-            </Modal.Header>
-
-            <Modal.Body>
-                <Form.Control className={`mb-2 ${styles.textArea}`}
-                    as="textarea"
-                    placeholder="Input Task"
-                />
-                <Form.Control
-                    className={styles.textArea}
-                    as="textarea"
-                    placeholder="Input Description"
-                />
-            </Modal.Body>
-        
-            <Modal.Footer className="d-flex justify-content-evenly">
-                <Button 
-                    variant='secondary'
-                    onClick={props.hideModal}
+                <Modal.Header 
+                    closeButton
+                    onClick={this.props.hideModal}
                 >
-                    Cancel
-                </Button>
-                <Button className='btn-modal-save-changes'>Save Changes</Button>
-            </Modal.Footer>
-        </Modal>
-    );
-}
+                    <Modal.Title className={styles.title}>Input changes</Modal.Title>
+                </Modal.Header>
+    
+                <Modal.Body>
+                    <Form.Control className={`mb-2 ${styles.textArea}`}
+                        as="textarea"
+                        placeholder="Input Task"
+                    />
+                    <Form.Control
+                        className={styles.textArea}
+                        as="textarea"
+                        placeholder="Input Description"
+                    />
+                </Modal.Body>
+            
+                <Modal.Footer className="d-flex justify-content-evenly">
+                    <Button 
+                        variant='secondary'
+                        onClick={this.props.hideModal}
+                    >
+                        Cancel
+                    </Button>
+                    <Button className='btn-modal-save-changes'>Save Changes</Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }       
+} 
+    
 
 export default EditableTask;
