e6d5bc057d9d9b4c8554c71c22e45762a1914b13
Hasmik <[hasmik.alikhanyan1@gmail.com]>
2023-05-17 14:51:38 +0400

    .

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -9,7 +9,7 @@ function About() {
             <div className={`mt-5 ${styles.aboutSection}`}>
                 <h1 className={styles.titles}>About Us Page</h1>
                 <p>ToDo list page is a list of tasks you need to complete or things that you want to do. 
-                A to do list acts as an external memory aid. Keep a to do list and you'll be able to keep track of everything, rather than just a few of the tasks you need to do. Your to do list will also reinforce the information, which makes it less likely you're going to forget something.
+                A to do list acts as an external memory aid. Keep a to do list and you'll be able to keep, track of everything, rather than just a few of the tasks you need to do. Your to do list will also reinforce the information, which makes it less likely you're going to forget something.
                 </p>
 
             <h2 className={`mt-4 ${styles.titles}`}>Our Team</h2>
